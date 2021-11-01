a677331bb6e3de606ded6525568a0bbdb60e7cd8
Thinh Le <letienthinh0109@gmail.com>
2021-11-02 03:37:41 +0700

    Demo v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bosch/fetch_and_show_test.dart';
 import 'package:bosch/provider/traffic_data.dart';
 import 'package:bosch/screen/splashing_screen.dart';
 import 'package:bosch/screen/stats_screen.dart';
@@ -21,15 +20,6 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'GREENPHL',
           theme: ThemeData(
-            // This is the theme of your application.
-            //
-            // Try running your application with "flutter run". You'll see the
-            // application has a blue toolbar. Then, without quitting the app, try
-            // changing the primarySwatch below to Colors.green and then invoke
-            // "hot reload" (press "r" in the console where you ran "flutter run",
-            // or simply save your changes to "hot reload" in a Flutter IDE).
-            // Notice that the counter didn't reset back to zero; the application
-            // is not restarted.
             primarySwatch: Colors.cyan,
             fontFamily: "Andada",
           ),
